096aa2f00297da6ce6f7a1b462f8f4741ca93e00
Femi Agbabiaka <femianthonyagbabiaka@gmail.com>
2017-01-14 14:00:39 -0600

    Fix docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
+/// shodan provides an ergonomic inteface for interacting with the shodan.io REST API.
+/// # Examples
+/// ```rust
+/// extern crate shodan;
+/// fn main() {
+/// use shodan::shodan::ShodanClient;
+/// let response = ShodanClient::new("INSERT_API_KEY")
+///  	.host_info("INSERT_IP_ADDRESS");
+/// }
+/// ```
+/// For more information on the Shodan API, visit [this link](https://developer.shodan.io/api).
 extern crate hyper;
 extern crate hyper_native_tls;
 
-/// shodan provides an ergonomic inteface for interacting with the shodan.io REST API.
-/// # Examples
-///
-/// ```
-/// extern crate shodan;
-/// use shodan::ShodanClient;
-/// ShodanClient::new("INSERT_API_KEY")
-///	.host_info("INSERT_IP_ADDRESS");
-/// ```
-/// For more information on the Shodan API, visit [this link](https://developer.shodan.io/api).
 
 pub mod shodan {
 
@@ -23,98 +24,97 @@ pub mod shodan {
     /// BaseUrl is the basis for all of our api requests.
     const BASE_URL: &'static str = "https://api.shodan.io";
 
-    #[derive(Debug)]
     pub struct ShodanClient {
-	api_key: &'static str,
+		api_key: &'static str,
     }
 
     impl ShodanClient {
-	fn create_http_client(&self) -> Client {
-	    let ssl = NativeTlsClient::new().unwrap();
-	    let connector = HttpsConnector::new(ssl);
-	    let client = Client::with_connector(connector);
+		fn create_http_client(&self) -> Client {
+			let ssl = NativeTlsClient::new().unwrap();
+			let connector = HttpsConnector::new(ssl);
+			let client = Client::with_connector(connector);
 
-	    client
-	}
-
-	fn request(&self, api_method: &str, url: String) -> Response {
-	    let client = self.create_http_client();
-
-	    let response = match api_method {
-		"GET" => {
-		    client.get(&*url)
-			.send()
-			.unwrap()
+			client
 		}
-		_ => panic!("Invalid HTTP Verb."),
-	    };
 
-	    response
-	}
+		fn request(&self, api_method: &str, url: String) -> Response {
+			let client = self.create_http_client();
 
-	fn form_response(&self, mut response: Response) -> Result<String, String> {
+			let response = match api_method {
+			"GET" => {
+				client.get(&*url)
+				.send()
+				.unwrap()
+			}
+			_ => panic!("Invalid HTTP Verb."),
+			};
 
-	    let mut body = String::new();
+			response
+		}
 
-	    if response.status == hyper_ok {
-		response.read_to_string(&mut body).unwrap();
+		fn form_response(&self, mut response: Response) -> Result<String, String> {
 
-		return Ok(body);
-	    }
+			let mut body = String::new();
 
-	    response.read_to_string(&mut body).unwrap();
+			if response.status == hyper_ok {
+			response.read_to_string(&mut body).unwrap();
 
-	    Err(body)
-	}
+			return Ok(body);
+			}
 
-	/// Creates a new ShodanClient.
-	pub fn new(api_key: &'static str) -> ShodanClient {
-	    ShodanClient { api_key: api_key }
-	}
+			response.read_to_string(&mut body).unwrap();
 
-	/// Method for `/shodan/host/{ip}`
-	pub fn host_info(&self, ip_address: &str) -> Result<String, String> {
-	    let formatted_url = format!("{}/shodan/host/{}?key={}",
-					BASE_URL,
-					ip_address,
-					self.api_key);
+			Err(body)
+		}
 
-	    let response = self.request("GET", formatted_url);
-	    self.form_response(response)
-	}
+		/// Creates a new ShodanClient.
+		pub fn new(api_key: &'static str) -> ShodanClient {
+			ShodanClient { api_key: api_key }
+		}
 
-	/// Method for `/shodan/host/search`, no facets.
-	pub fn search(&self, query: &str) -> Result<String, String> {
-	    let formatted_url = format!("{}/shodan/host/search?key={}&query={}",
-					BASE_URL,
-					self.api_key,
-					query);
+		/// Method for `/shodan/host/{ip}`
+		pub fn host_info(&self, ip_address: &str) -> Result<String, String> {
+			let formatted_url = format!("{}/shodan/host/{}?key={}",
+						BASE_URL,
+						ip_address,
+						self.api_key);
 
-	    let response = self.request("GET", formatted_url);
-	    self.form_response(response)
-	}
+			let response = self.request("GET", formatted_url);
+			self.form_response(response)
+		}
 
-	/// Method for `/shodan/host/search`, with facets.
-	pub fn search_with_facets(&self, query: &str, facets: &str) -> Result<String, String> {
-	    let formatted_url = format!("{}/shodan/host/search?key={}&query={}&facets={}",
-					BASE_URL,
-					self.api_key,
-					query,
-					facets);
+		/// Method for `/shodan/host/search`, no facets.
+		pub fn search(&self, query: &str) -> Result<String, String> {
+			let formatted_url = format!("{}/shodan/host/search?key={}&query={}",
+						BASE_URL,
+						self.api_key,
+						query);
 
-	    let response = self.request("GET", formatted_url);
-	    self.form_response(response)
-	}
+			let response = self.request("GET", formatted_url);
+			self.form_response(response)
+		}
 
-	/// Method for `/labs/honeyscore/{ip}`.
-	pub fn honeyscore(&self, ip_address: &str) -> Result<String, String> {
-	    let formatted_url = format!("{}/labs/honeyscore/{}?key={}",
-					BASE_URL,
-					ip_address,
-					self.api_key);
+		/// Method for `/shodan/host/search`, with facets.
+		pub fn search_with_facets(&self, query: &str, facets: &str) -> Result<String, String> {
+			let formatted_url = format!("{}/shodan/host/search?key={}&query={}&facets={}",
+						BASE_URL,
+						self.api_key,
+						query,
+						facets);
 
-	    let response = self.request("GET", formatted_url);
-	    self.form_response(response)
-	}
+			let response = self.request("GET", formatted_url);
+			self.form_response(response)
+		}
+
+		/// Method for `/labs/honeyscore/{ip}`.
+		pub fn honeyscore(&self, ip_address: &str) -> Result<String, String> {
+			let formatted_url = format!("{}/labs/honeyscore/{}?key={}",
+						BASE_URL,
+						ip_address,
+						self.api_key);
+
+			let response = self.request("GET", formatted_url);
+			self.form_response(response)
+		}
     }
 }
